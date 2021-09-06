801da2b0b0063e849d7162888540bc867f9b6d0d
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2021-09-06 12:04:16 +0200

    fixup! Add purge CSS

--- postcss.config.js
+++ postcss.config.js
@@ -17,25 +17,29 @@ module.exports = {
         },
       },
     ],
+
     [
       "@fullhuman/postcss-purgecss",
-      {
-        content: [
-          "./components/**/*.{js,ts,jsx,tsx}",
-          "./data/**/*.{js,ts,jsx,tsx}",
-          "./pages/**/*.{js,ts,jsx,tsx}",
-          "./lib/**/*.{js,ts,jsx,tsx}",
-        ],
-        safelist: [/fa(.*)/, /svg(.*)/, "hidden", "html", "body"],
-        defaultExtractor: (content) => {
-          // Capture as liberally as possible, including things like `h-(screen-1.5)`
-          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-          // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-          const innerMatches =
-            content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-          return broadMatches.concat(innerMatches);
-        },
-      },
+      enablePurgeCSS
+        ? {
+            content: [
+              "./components/**/*.{js,ts,jsx,tsx}",
+              "./data/**/*.{js,ts,jsx,tsx}",
+              "./pages/**/*.{js,ts,jsx,tsx}",
+              "./lib/**/*.{js,ts,jsx,tsx}",
+            ],
+            safelist: [/fa(.*)/, /svg(.*)/, "hidden"],
+            defaultExtractor: (content) => {
+              // Capture as liberally as possible, including things like `h-(screen-1.5)`
+              const broadMatches =
+                content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+              // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+              const innerMatches =
+                content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+              return broadMatches.concat(innerMatches);
+            },
+          }
+        : false,
     ],
   ],
 };
