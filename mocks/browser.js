d755bf3695879ab2cd0531606f9267f8a6775ec8
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2021-09-07 12:38:48 +0200

    Fix infinite refresh when MockDate was set

--- mocks/browser.js
+++ mocks/browser.js
@@ -1,4 +1,11 @@
 import { setupWorker } from "msw";
 import { handlers } from "./handlers";
+import MockDate from "mockdate";
+
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  // If MSW is active we want to stop the time at 25 of August for the stored
+  // mocks to work
+  MockDate.set("2021-08-25");
+}
 
 export const worker = setupWorker(...handlers);
