4064d53e1f74eb7909c2bbf40fdb423dd8208407
Davis Norde <davis.norde@gmail.com>
2025-10-29 14:31:13 +0200

    Use the Link component in Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -6,9 +6,12 @@ const Header = ({ title }) => {
     <header className="bg-white shadow-sm">
       <div className="container flex justify-between items-center py-3">
         <nav className="flex space-x-2 sm:space-x-4">
-          <a className="flex items-center no-underline font-semibold" href="/">
+          <Link
+            href="/"
+            className="flex items-center no-underline font-semibold"
+          >
             {title}
-          </a>
+          </Link>
           <div className="h-6 w-px bg-gray-200 rounded" />
           <Link href="/" className="no-underline text-gray-700">
             Status
