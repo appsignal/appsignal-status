752548d16a364264405d8680a420c74d7e8d6ee7
Goran Martinjak Radalj <goran@goran.im>
2026-07-02 15:14:01 +0200

    Bump Next.js to 16 and React to 19 (#203)
    * Bump Next.js to 16 and React to 19
    
    * Bump eslint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-const path = require("path");
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias["react"] = path.resolve("./node_modules/react");
-    return config;
-  },
 };
